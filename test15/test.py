bbb3263c2c3eaa36ce47780351c89a56f9e7ad09
Kei Okada <kei.okada@gmail.com>
2015-02-16 11:05:28 +0900

    initial commit

---
+++ test15/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import unittest1
+
+class TestBareBones(unittest1.TestCase):
+    def test1_one_equals_one(self):
+        self.assertEquals(1,1,"1!=1")
+        
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun('test15', 'test_bare_bones', TestBareBones)
---
+++ test21/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import unittest2
+
+class TestBareBones(unittest2.TestCase):
+    def test2_one_equals_one(self):
+        self.assertEquals(1,1,"1!=1")
+        
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun('test21', 'test_bare_bones', TestBareBones)
